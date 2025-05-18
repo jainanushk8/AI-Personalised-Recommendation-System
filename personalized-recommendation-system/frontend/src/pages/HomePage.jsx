5e8d59a7f0a90610780b36094a49df613db21ade
Anushk Jain <jainanushk8@gmail.com>
2025-05-18 15:22:04 +0530

    changes

---
+++ personalized-recommendation-system/frontend/src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+    return <div>Welcome to the Personalized Recommendation System!</div>;
+};
+
+export default HomePage;
---
+++ personalized-recommendation-system/frontend/src/pages/RecommendationPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecommendationPage = () => {
+    return <div>Here are your personalized recommendations!</div>;
+};
+
+export default RecommendationPage;
